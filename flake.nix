3609edc315c72f9a851e45214655c1d90c8bee67
Markus Bandion <m.bandion@invitario.com>
2023-04-29 20:57:02 +0200

    add nix env configuration

---
+++ flake.nix
@@ -0,0 +1,43 @@
+{
+   description = "Development environment";
+
+   inputs = {
+      nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
+      flake-utils.url = "github:numtide/flake-utils";
+   };
+
+   outputs = { self, nixpkgs, flake-utils }:
+      flake-utils.lib.eachDefaultSystem
+         (system:
+            let
+               pkgs = import nixpkgs {
+                  inherit system;
+               };
+
+               inputs = [
+                  pkgs.ripgrep
+                  pkgs.git-up
+                  pkgs.nodejs-18_x
+               ];
+
+               shellHooks = ''
+                  mkdir -p .nix-node
+                  export NODE_PATH=$PWD/.nix-node
+                  export NPM_CONFIG_PREFIX=$PWD/.nix-node
+                  export PATH=$NODE_PATH/bin:$PATH
+                  
+                  npm install --global editorconfig-checker
+               '';
+            in
+            {
+               devShells = {
+                  default = pkgs.mkShellNoCC {
+                     name = "tomtomsen dotfiles";
+
+                     buildInputs = inputs;
+                     shellHook = shellHooks;
+                  };
+               };
+            }
+         );
+}
